1d1b5d7e0d7b54d0b647c96665882a52fcb27e31
ds-aachuki <aachuki.agarwal@dotsquares.com>
2025-07-16 11:12:37 +0530

    add permission for package manager in seeder

--- database/seeders/AdminPermissionSeeder.php
+++ database/seeders/AdminPermissionSeeder.php
@@ -14,6 +14,7 @@ class AdminPermissionSeeder extends Seeder
     {
         $permissions = [
             ['name' => 'Dashboard', 'slug' => 'dashboard'],
+            ['name' => 'Package Manager List', 'slug' => 'package_manager_list'],
 
             // Admin Manager
             ['name' => 'Admin Manager List', 'slug' => 'admin_manager_list'],
